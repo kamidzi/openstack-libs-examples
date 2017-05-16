342f9d381d2dcf8b3e1cfcfddf8b4ccdf3a772d0
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2017-05-16 14:26:59 -0400

    WIP: Avoid using keystoneauth1 for pre-Mitaka ubuntu distros

--- examples/keystonemiddleware/glance_images.py
+++ examples/keystonemiddleware/glance_images.py
@@ -3,9 +3,10 @@
 # http://www.jamielennox.net/blog/2015/09/10/user-auth-in-openstack-services
 from glanceclient import client
 import json
-from keystoneauth1 import loading
-from keystoneauth1 import session
 from keystoneclient import client as ks_client
+from keystoneclient import auth as ks_auth
+from keystoneclient import session
+from keystoneclient.auth.identity import v2, v3
 from keystonemiddleware import auth_token
 from oslo_config import cfg
 import webob.dec
@@ -15,10 +16,12 @@ import sys
 
 CONF = cfg.CONF
 CONF(project='testservice')
-loading.session.register_conf_options(cfg.CONF, 'communication')
-vars = filter(lambda x: x[0].startswith('OS_'), os.environ.iteritems())
+#print(map(lambda x: x.name, ks_auth.get_common_conf_options()))
+#print(map(lambda x: x.name, ks_auth.get_plugin_options('v3password')))
+v3.Password.register_conf_options(CONF, 'communication')
+_vars = filter(lambda x: x[0].startswith('OS_'), os.environ.iteritems())
 conf_keys = CONF.keys()
-for k, v in vars:
+for k, v in _vars:
 # Try the full var first
     n = k.lower()
     cands = (n, n[3:])
@@ -31,7 +34,16 @@ CONF(sys.argv[1:])
 
 # TODO: --help doesn't seem to work either way...
 # Doesn't seem to matter whether keystoneclient.session.Session or keystoneauth1.session.Session
-SESSION = loading.session.load_from_conf_options(cfg.CONF, 'communication')
+auth_args = {
+    'auth_url': CONF.communication.auth_url,
+    'username': CONF.communication.username,
+    'password': CONF.communication.password,
+    'project_name': CONF.communication.project_name,
+    'project_domain_name': CONF.communication.project_domain_name
+}
+#auth = v3.Password(auth_args['auth_url'],**auth_args)
+auth = v3.Password(**auth_args)
+SESSION = session.Session(auth=auth)
 
 @webob.dec.wsgify
 def app(req):
@@ -39,7 +51,12 @@ def app(req):
 # 
 # DiscoveryFailure: Not enough information to determine URL. Provide either a Session, or auth_url or endpoint
 # Loaded session will not set 'auth' attribute
-    SESSION.auth = req.environ['keystone.token_auth']._auth
+    # TODO(kamidzi): need to pick this apart
+    SESSION.auth = req.environ['keystone.token_auth']._user_auth_ref
+    print(type(req.environ['keystone.token_auth']))
+    for k, v in req.environ['keystone.token_auth'].iteritems():
+        print('{}: {}'.format(k, type(v)))
+
     kclient = ks_client.Client('3', session=SESSION)
     projects = kclient.projects.list()
 
@@ -64,8 +81,9 @@ if __name__ == '__main__':
     import sys
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
     logger = logging.getLogger(cfg.CONF.project)
+    CONF.log_opt_values(logger, lvl=logging.INFO)
 
     app = auth_token.AuthProtocol(app,{})
-    server = simple_server.make_server('', 8000, app)
+    server = simple_server.make_server('', 8001, app)
     server.handle_request()
     server.serve_forever()
